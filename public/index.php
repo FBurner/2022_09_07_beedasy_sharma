79593a15cd2154f8a647badbff2e7ba17f11f88b
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-07 09:47:01 +0200

    Regular auto-commit at 09:47:01 on 07/09/2022

--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
 <?php
+$db = 'c24.blog';
+$user = 'c24.blog';
+$password = 'check24.de';
+$conn = new mysqli('p:localhost', $user, $password, $db);
 
+if (!$conn->connect_errno) {
+    $conn->set_charset('utf8');
+    if ($conn->select_db($db)) {
+        $mysqlVersion = $conn->get_server_info();
+        echo "<p class='good'>Success, <b>MySql server</b> version $mysqlVersion</p>";
+    } else echo "<p class='bad'>Unable to select database <b><?=$db?></b></p>";
+} else echo "<p class='bad'>Unable to connect to <b>MySql server</b></p>";
 
-require_once '../src/bootstrap.php';
 
