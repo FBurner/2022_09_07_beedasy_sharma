d5d7fcaeccdcdff3a2cdd9ff2bd78fa2f7f85874
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 11:51:01 +0200

    Regular auto-commit at 11:51:01 on 08/09/2022

--- app/Service/App.php
+++ app/Service/App.php
@@ -1,14 +1,23 @@
 <?php
 
+
+
+namespace App\Service;
+
+use Bramus\Router\Router;
 use Psr\Container\ContainerInterface;
 
 class App
 {
     private ContainerInterface $container;
 
-    public function __construct(DI\Container $container)
+    private Router $router;
+
+    public function __construct(DI\Container $container, Router $router)
     {
         $this->container = $container;
+
+        $this->router = $router;
     }
 
     public function run(): void
