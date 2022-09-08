6b93a8b2c2260b73aa46507f213bb7493a9abb81
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 14:37:00 +0200

    Regular auto-commit at 14:37:00 on 08/09/2022

--- app/Service/Db.php
+++ app/Service/Db.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use PDO;
+
 class Db
 {
+    private
     public function __construct(string $host, string $username, string $password)
     {
-
+        $pdo = new PDO('mysql:host=localhost;dbname=myDatabase;charset=utf8mb4', "username", "password", [
+            PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+        ]);
     }
 }
