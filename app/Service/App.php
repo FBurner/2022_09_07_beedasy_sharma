a8ef3993cde322c0bce45e88d1081a38825264c8
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 11:45:01 +0200

    Regular auto-commit at 11:45:01 on 08/09/2022

--- app/Service/App.php
+++ app/Service/App.php
@@ -1,6 +1,13 @@
 <?php
 
+use Psr\Container\ContainerInterface;
+
 class App
 {
+    private ContainerInterface $container;
 
+    public function __construct(DI\Container $container)
+    {
+        $this->container = $container;
+    }
 }
