c41a213ea603aeab7ae1b1673f923699481ae76b
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 14:10:01 +0200

    Regular auto-commit at 14:10:01 on 08/09/2022

--- routes.php
+++ routes.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Controller\PostController;
 use App\Service\Router;
 
 $router = new Router();
 
-$router->setNamespace('\App\Controller');
-
-$router->match('GET', '/', ['PostController', 'index']);
+$router->match('GET', '/', [PostController::class, 'index']);
 
 return $router;
