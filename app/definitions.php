3b0ed4d3201bbe085d46bfb40e866be3a7339aeb
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 13:32:01 +0200

    Regular auto-commit at 13:32:01 on 08/09/2022

--- app/definitions.php
+++ app/definitions.php
@@ -1,1 +1,12 @@
 <?php
+
+declare(strict_types=1);
+
+use App\Service\Db;
+use Psr\Container\ContainerInterface;
+
+return [
+    'Db' => function(ContainerInterface $c) {
+        return new Db();
+    }
+];
