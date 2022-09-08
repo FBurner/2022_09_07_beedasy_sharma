f79ef8544bd665f8cd76d421710bc73b63b0cd67
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 14:25:01 +0200

    Regular auto-commit at 14:25:01 on 08/09/2022

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,7 @@
 
 use App\Contracts\Router;
 
-function routes(Router $router) {
-
+function routes(Router $router): void
+{
+    $router->match('GET', '/', ['App\Controller\PostController', 'index']);
 }
