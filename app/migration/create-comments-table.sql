0de09288ff153946042dc861384892d91d4ec5b1
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 14:50:00 +0200

    Regular auto-commit at 14:50:00 on 08/09/2022

---
+++ app/migration/create-comments-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS comments
+(
+    comment_id INT NOT NULL AUTO_INCREMENT,
+    title   varchar(1000),
+    body    text
+)
--- app/migration/create-posts-table.sql
+++ app/migration/create-posts-table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS posts
 (
     post_id INT NOT NULL AUTO_INCREMENT,
     title   varchar(1000),
-    text    text
+    body    text
 )
