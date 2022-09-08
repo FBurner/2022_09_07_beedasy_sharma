e1422295bf02e364ed657a5ac3eb1cfb4738924a
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 14:54:00 +0200

    Regular auto-commit at 14:54:00 on 08/09/2022

--- app/migration/create-posts-table.sql
+++ app/migration/create-posts-table.sql
@@ -3,5 +3,8 @@ CREATE TABLE IF NOT EXISTS posts
     post_id INT NOT NULL AUTO_INCREMENT,
     title   varchar(1000),
     body    text,
-    user_id INT NOT NULL 
+    user_id INT,
+    FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+        ON DELETE SET NULL
 )
