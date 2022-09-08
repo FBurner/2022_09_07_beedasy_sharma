29271e61679fc64e5ad7a94cb8a7d606a3192885
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 14:12:01 +0200

    Regular auto-commit at 14:12:01 on 08/09/2022

--- app/Controller/PostController.php
+++ app/Controller/PostController.php
@@ -3,9 +3,17 @@
 namespace App\Controller;
 
 use App\Service\Controller;
+use App\Service\PostService;
 
 class PostController extends Controller
 {
+    private PostService $postService;
+
+    public function __construct(PostService $postService)
+    {
+        $this->postService = $postService;
+    }
+
     public function index(): void
     {
         echo 'lol';
