d14a3f3d406f0e858832cf2cc25ea3a9e21da60c
Bewerber <Bewerber@PC-S4EY3950.check24.intern>
2022-09-08 13:56:01 +0200

    Regular auto-commit at 13:56:01 on 08/09/2022

--- app/definitions.php
+++ app/definitions.php
@@ -8,5 +8,8 @@ use Psr\Container\ContainerInterface;
 return [
     'Db' => function(ContainerInterface $c) {
         return new Db();
+    },
+    'Twig' => function(ContainerInterface $c) {
+        $loader = new Twig_Loader_Filesy(__DIR__.'/../templates');
     }
 ];
